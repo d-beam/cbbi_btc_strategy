ff34b6178f42813318dfb8c591c868f23edbd0db
d-beam <dbmueller@gmx.net>
2024-03-27 21:39:12 +0000

    Add Streamlit app files

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ st.title('CBBI BTC Strategy Dashboard')
 
 st.markdown("""
 Welcome to the CBBI BTC Strategy Dashboard! Navigate through the tabs to access different tools.
+Außerdem habe ich das noch hinzugefügt.
 """)
